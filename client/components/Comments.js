f59d16f06b36bbd0bb191db3336ddfaea5be7522
Saujanya Nagpal <saujanyanagpal104@gmail.com>
2018-06-29 00:53:44 +0530

    displaying single photo component

---
+++ client/components/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return(
+      <div class="comment">
+        I am the comment
+      </div>
+    )
+  }
+});
+export default Comments;
